fcf56904ae1b597962ddba176e63b31565ec6aef
Gitax18 <lusifer0418@gmail.com>
2025-04-22 14:09:56 +0530

    add local dev add

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "https://arcane-hollows-46188-ea0f5f3f003d.herokuapp.com/";
+axios.defaults.baseURL = "http://localhost:7070";
 axios.defaults.withCredentials = true;
 
 export default axios;
