37a65cf7b9caf320f549fd3d94db1906ef601618
Gitax18 <lusifer0418@gmail.com>
2025-04-26 23:01:33 +0530

    add delete account btn

---
+++ src/components/delete-btn.tsx
@@ -0,0 +1,36 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+function DeleteButton({ onContinue }: { onContinue: () => void }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger className="bg-red-500  font-bold tracking-wide py-2 px-5 rounded-md cursor-pointer">
+        Delete
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
+
+export default DeleteButton;
