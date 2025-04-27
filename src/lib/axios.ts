8f4a30656a9880766debf15bde91c695cd1a7b85
Gitax18 <lusifer0418@gmail.com>
2025-04-27 10:48:11 +0530

    correct path

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:7070";
+axios.defaults.baseURL =
+  "https://arcane-hollows-46188-ea0f5f3f003d.herokuapp.com/";
 axios.defaults.withCredentials = true;
 
 export default axios;
